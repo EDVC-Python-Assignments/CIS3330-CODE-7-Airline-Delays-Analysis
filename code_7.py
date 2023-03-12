b209590ffa3eda024770577dfcc6081b80a8a914
davicompu <villacis@vt.edu>
2023-03-11 18:44:52 -0700

    Update code_7.py

--- code_7.py
+++ code_7.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+#If any of this libraries is missing from your computer. Please install them using pip.
 
 filename = 'Flight_Delays_2018.csv'
 
